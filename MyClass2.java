2e1c12eac806ff38427ec861945de8a0b2badb56
harshaworks11 <70839166+harshaworks11@users.noreply.github.com>
2021-12-02 10:45:55 +0530

    Add files via upload

---
+++ MyClass2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author harsh
+ */
+public class MyClass2 {
+    public static void main(String args[])
+    {
+        String str = "welcome";
+        System.out.println("Hello World!" + str);
+    }
+    
+}
