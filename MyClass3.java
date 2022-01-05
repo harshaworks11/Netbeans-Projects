526a020003aaef364ba7a9ffcf7750f5391c1a13
harshaworks11 <70839166+harshaworks11@users.noreply.github.com>
2022-01-05 14:34:18 +0530

    Add files via upload

---
+++ MyClass3.java
@@ -0,0 +1,6 @@
+public class MyClass3{
+    public static void main(String args[]){
+        System.out.println("Welcome to netbeans Projects");
+        System.out.println("this is the third project ");
+    }
+}
